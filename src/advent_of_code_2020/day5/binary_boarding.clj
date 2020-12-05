92ce45697437a89663dfba28bd246b0ea94b184b
Prateek Jain <prateek.jain23995@yahoo.com>
2020-12-05 22:17:35 +0800

    day 5 p1

---
+++ src/advent_of_code_2020/day5/binary_boarding.clj
@@ -0,0 +1,32 @@
+(ns advent-of-code-2020.day5.binary-boarding
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def round-down (comp int #(Math/floor %)))
+(def round-up (comp int #(Math/ceil %)))
+
+(defn decode-symbol [input left-sym right-sym]
+  (loop [left 0
+         right (- (Math/pow 2 (count input)) 1)
+         seq input]
+    (if (empty? seq)
+      left
+      (let [center (-> (+ left right)
+                       (/ 2))]
+        (condp = (first seq)
+          left-sym (recur left (round-down center) (rest seq))
+          right-sym (recur (round-up center) right (rest seq)))))))
+
+(defn seat-id [input]
+  (let [parsed-input (str/split input #"")
+        row (decode-symbol (take 7 parsed-input) "F" "B")
+        column (decode-symbol (take-last 3 parsed-input) "L" "R")]
+    (-> 8
+        (* row)
+        (+ column))))
+
+(defn -main [& args]
+  (println (->> (slurp (io/resource "day5-input.txt"))
+                str/split-lines
+                (map seat-id)
+                (apply max))))
