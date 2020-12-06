84a6d2488f644f11bdc5ccd22c045204691609b4
Prateek Jain <prateek.jain23995@yahoo.com>
2020-12-06 13:56:52 +0800

    day 5 completed

--- src/advent_of_code_2020/day5/binary_boarding.clj
+++ src/advent_of_code_2020/day5/binary_boarding.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2020.day5.binary-boarding
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def round-down (comp int #(Math/floor %)))
 (def round-up (comp int #(Math/ceil %)))
@@ -25,8 +26,26 @@
         (* row)
         (+ column))))
 
+(defn find-max-seat-id [input]
+  (->> input
+       (map seat-id)
+       (apply max)))
+
+(defn find-my-seat [input]
+  (let [row-with-missing-seat (->> (group-by #(subs % 0 7) input)
+                                   (filter #(= 7 (count (second %))))
+                                   (into {}))
+        row (decode-symbol (-> row-with-missing-seat keys first (str/split #"")) "F" "B")
+        all-rows-seat (set (map #(-> 8
+                                     (* row)
+                                     (+ %)) (range 0 8)))]
+    (->> (map seat-id (-> row-with-missing-seat vals first))
+         set
+         (set/difference all-rows-seat)
+         first)))
+
 (defn -main [& args]
-  (println (->> (slurp (io/resource "day5-input.txt"))
-                str/split-lines
-                (map seat-id)
-                (apply max))))
+  (let [input (->> (slurp (io/resource "day5-input.txt"))
+                   str/split-lines)]
+    (println (find-max-seat-id input))
+    (println (find-my-seat input))))
